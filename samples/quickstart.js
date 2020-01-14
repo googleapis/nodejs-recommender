3d26af9be3625926d2b63cac4806889932f37162
Benjamin Coe <bencoe@google.com>
2020-01-14 11:56:31 -0800

    chore: get sample actually working

--- samples/quickstart.js
+++ samples/quickstart.js
@@ -1,36 +1,39 @@
-/**
- * Copyright 2019, Google LLC.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 
 'use strict';
 
 /**
- * DESCRIBE WHAT THIS SAMPLE DOES.
- * @param {string} LIST EXPECTED ARGUMENTS.
+ * List usage recommendations for a given product.
+ * @param {string} project
+ * @param {string} recommenderName
  */
-async function main(project = 'my-project') {
+async function main(
+  project = 'my-project',
+  recommenderId = 'google.compute.instance.MachineTypeRecommender'
+) {
   // [START recommender_quickstart]
   async function listRecommendations() {
     const {RecommenderClient} = require('@google-cloud/recommender');
-    const recommender = new RecommenderClient({
-      projectId: project,
-    });
+    const recommender = new RecommenderClient();
 
-    // parent = 'projects/my-project', // Project to fetch recommendations for.
+    // parent = 'projects/my-project'; // Project to fetch recommendations for.
+    // recommenderId = 'google.compute.instance.MachineTypeRecommender';
 
     const [recommendations] = await recommender.listRecommendations({
-      parent: `projects/${project}`,
+      parent: recommender.recommenderPath(project, 'global', recommenderId),
     });
     console.info(recommendations);
     return recommendations;
